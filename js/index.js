de550f438ec67ccea17328723b97ffe03e0672fe
David <David@marys-macbook-pro.local>
2017-08-29 05:47:27 -0500

    Stripe changes color to red as it moves

--- js/index.js
+++ js/index.js
@@ -2,8 +2,11 @@
 https://cdnjs.cloudflare.com/ajax/libs/gsap/1.20.2/TweenMax.min.js
 */
 
-
+// Moves shoe to right
 window.onload = function(){
  TweenLite.to("#wrapper", 10, {left:600});
 }
 
+
+// CSS plug in changes CSS fill on stripe to red over time
+TweenLite.to("#stripe_3", 2, {css:{fill:"red"}, ease:Power2.easeOut});
